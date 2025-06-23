99ea6d2c4da9b68a43f1b7f81013493971d186a7
Akash <chakash793@gmail.com>
2025-06-23 21:03:38 +0530

    Minor Changes

--- src/components/homepage/Pricing.tsx
+++ src/components/homepage/Pricing.tsx
@@ -3,10 +3,8 @@ import PremiumCard from "./cards/PremiumCard"
 
 const Pricing = () => {
   return (
-    <>
-      <div className="w-full flex h-auto min-h-20 mb-10 md:mb-20 bg-[#F9FAFB] px-4 py-6 md:py-0">
-        <p className="w-full flex items-center justify-center text-center text-xl sm:text-2xl">Your bio link is like a blind date—no idea who clicked. Let's change that.</p>
-      </div>
+    
+      
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 min-h-screen py-8 flex flex-col items-center justify-center">
         <div className="flex flex-col justify-center items-center text-center mb-8">
           <h1 className="text-2xl md:text-5xl font-bold">Choose Your Perfect Plan</h1>
@@ -18,7 +16,7 @@ const Pricing = () => {
           <PremiumCard/>
         </div>
       </div>
-    </>
+    
   )
 }
 
